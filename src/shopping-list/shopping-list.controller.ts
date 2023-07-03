14b421b2626cf2450ec1c75330b651e698c268f7
Sven <sven.ole.lux1991@gmail.com>
2023-07-03 21:51:30 +0200

    added import

--- src/shopping-list/shopping-list.controller.ts
+++ src/shopping-list/shopping-list.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ShoppingListService } from './shopping-list.service';
 import { CreateShoppingListDto } from './dto/create-shopping-list.dto';
 import { UpdateShoppingListDto } from './dto/update-shopping-list.dto';
+import { ShoppingItem } from './schemas/shopping-list.schema';
 
 @Controller('shopping-list')
 export class ShoppingListController {
