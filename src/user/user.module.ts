68e4651410c67ecde0c732037202a598ff54ff12
Sven <sven.ole.lux1991@gmail.com>
2023-07-14 19:46:46 +0200

    fixed imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserItem, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserItem.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
