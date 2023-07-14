0bbf979778fd80cb1383958577c25406bbf8c69a
Sven <sven.ole.lux1991@gmail.com>
2023-07-14 22:36:18 +0200

    added update dto

---
+++ src/shopping-list/dto/update-shopping-list.-user-dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateShoppingListDto } from './create-shopping-list.dto';
+
+export class UpdateShoppingListUserDto  {
+  assignedUser: string
+}
