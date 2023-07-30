39c60228631b6f0f65daf540b4856674a5cff736
Sven <sven.ole.lux1991@gmail.com>
2023-07-30 23:36:29 +0200

    added module

--- src/food/food.module.ts
+++ src/food/food.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FoodService } from './food.service';
 import { FoodController } from './food.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FoodItem, FoodItemSchema } from './schemas/food.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: FoodItem.name, schema: FoodItemSchema },
+    ]),
+  ],
   controllers: [FoodController],
   providers: [FoodService]
 })
