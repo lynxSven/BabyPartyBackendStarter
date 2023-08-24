86139dd43978216047e5331be54e15fc6d4c52a3
Sven <sven.ole.lux1991@gmail.com>
2023-08-24 10:01:16 +0200

    delete user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UserItem, UserSchema } from './schemas/user.schema';
 
 @Injectable()
@@ -27,7 +28,11 @@ export class UserService {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  async remove(id: string): Promise<UserItem> {
+
+    const deleted = await this.userItemModel
+    .findByIdAndRemove({ _id: id })
+    .exec();
+  return deleted;
+     }
 }
